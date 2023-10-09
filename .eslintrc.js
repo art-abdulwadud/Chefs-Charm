2c060bb7260dfab57e68dd471fe8a9154efa40ec
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-09 22:58:15 +0300

    refactor(linting): add specific eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,89 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'airbnb'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
-    'react',
+    'react'
   ],
   rules: {
-  },
+    'comma-dangle': ['error', 'never'],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false
+      }
+    ],
+    radix: 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-underscore-dangle': 0,
+    'no-warning-comments': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'require-unicode-regexp': 0,
+    'prefer-named-capture-group': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'react/no-unescaped-entities': 0,
+    'react/function-component-definition': 0,
+    'react/display-name': 0,
+    'react/jsx-no-useless-fragment': 0,
+    'import/no-cycle': 0,
+    'class-methods-use-this': 0,
+    'no-invalid-this': 0,
+    'react/prop-types': 0,
+    'react/jsx-indent': ['error', 2],
+    'spaced-comment': 0,
+    'max-lines': 0,
+    'import/no-named-as-default': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    'default-param-last': 0,
+    'import/prefer-default-export': 0,
+    'array-element-newline': 0,
+    'multiline-ternary': 0,
+    'no-magic-numbers': 0,
+    'arrow-body-style': 0,
+    'no-alert': 0,
+    'eslint(quotes)': 0,
+    'max-lines-per-function': 0,
+    'object-curly-newline': 0,
+    'no-unused-expressions': 0,
+    'no-confusing-arrow': 0,
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
+    'no-useless-constructor': 0,
+    'object-shorthand': 0,
+    'no-ternary': 0,
+    'sort-keys': 0,
+    'max-len': 0,
+    'no-console': 0,
+    'no-return-assign': 0,
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': 0,
+    'no-nested-ternary': 0,
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: true,
+        memberSyntaxSortOrder: ['none',
+          'all',
+          'multiple',
+          'single']
+      }
+    ]
+  }
 };
