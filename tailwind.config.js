9059478ac77e41b427e9f83968e510b94b105d3a
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-14 02:36:42 +0300

    feat(tailwind): configure tailwind to just-in-time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode: true,
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}'
