46702e23e0ba07ab664a90f429023482d493c10e
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-14 01:45:59 +0300

    change favicon in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,12 +26,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: '#ffcc00',
+        theme_color: '#ffcc00',
         display: 'minimal-ui',
-        icon: 'static/logo-tp.png' // This path is relative to the root of the site.
+        icon: 'static/favicon.png'
       }
     }
   ]
