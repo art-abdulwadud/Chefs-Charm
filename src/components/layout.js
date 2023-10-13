4045f60b2e8b3706101d0d73007fefdb4ca78931
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-14 02:29:38 +0300

    remove prime packages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import 'animate.css/animate.min.css';
-import 'primereact/resources/themes/saga-orange/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 import './layout.css';
 
 const Layout = ({ children }) => {
