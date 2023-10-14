bc34ec2367961e16ffcd143e3d442c3aa01c0f6a
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-14 21:52:16 +0300

    style(navbar): add colors, shodows and border radius to navitems for large screens

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -10,14 +10,14 @@ const NavBar = () => {
           <NavToggler />
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img className="h-8 w-auto" src="/logo-tp.png" alt="logo" />
+              <img className="h-9 w-10" src="/logo-tp.png" alt="logo" />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                <NavItemLG label="Dashboard" active />
+                <NavItemLG label="Team" />
+                <NavItemLG label="Projects" />
+                <NavItemLG label="Calendar" />
               </div>
             </div>
           </div>
@@ -25,10 +25,10 @@ const NavBar = () => {
       </div>
       <div className="sm:hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <NavItemLG label="Dashboard" />
-          <NavItemLG label="Team" />
-          <NavItemLG label="Projects" />
-          <NavItemLG label="Calendar" />
+          <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
         </div>
       </div>
     </nav>
--- src/components/navbar/NavItemLG.js
+++ src/components/navbar/NavItemLG.js
@@ -1,14 +1,15 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
-const NavItemLG = ({ label }) => {
+const NavItemLG = ({ label, active }) => {
   return (
-    <a
-      href="#"
-      className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+    <Link
+      to="/"
+      className={`${active ? 'bg-gray-800 text-amber-400 shadow-xl shadow-gray-800/40' : 'text-gray-800 hover:bg-gray-800 hover:text-white'} block rounded-b-lg px-3 py-2 text-base font-medium`}
       aria-current="page"
     >
       {label}
-    </a>
+    </Link>
   );
 };
 
