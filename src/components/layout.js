b32fccdc464986c08862f102365262428e9eba9e
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-15 04:43:23 +0300

    add ubuntu google font

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import 'animate.css/animate.min.css';
 import './layout.css';
+import './fonts.css';
 import NavBar from './navbar/NavBar';
 
 const Layout = ({ children }) => {
