d04e3d57f47c0308dd34042a004c3eb0ae1ef046
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-16 17:40:07 +0300

    style(navbar): change colors, backgrounds and borders for navitems in large screen

--- src/components/navbar/NavItemLG.js
+++ src/components/navbar/NavItemLG.js
@@ -8,7 +8,7 @@ const NavItemLG = ({ label, active }) => {
       className={`
         ${active
         ? 'dark:bg-gray-800 bg-white text-amber-400 shadow-2xl shadow-gray-800/80'
-        : 'dark:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-white text-white hover:bg-gray-50 hover:text-gray-500 hover:shadow-2xl hover:shadow-gray-800/80 cursor-pointer'} 
+        : 'dark:text-gray-800 dark:hover:border-gray-800 text-white hover:border-white hover:border-solid hover:border hover:shadow-2xl hover:shadow-gray-800/80 cursor-pointer'} 
         block rounded-b-lg px-3 py-2 text-base font-medium transition-all duration-300`}
       aria-current="page"
     >
