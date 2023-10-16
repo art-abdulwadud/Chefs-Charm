95dd93a34140551c5608bee886e71284bffd1b56
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-16 21:59:40 +0300

    style(navbar): add hover animation to navitem for large screens

--- src/components/navbar/NavItemLG.js
+++ src/components/navbar/NavItemLG.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 const NavItemLG = ({ label, active }) => {
   return (
-    <motion.a
-      whileHover={{ ...active ? {} : { rotate: 360 } }}
-      to="/"
-      className={`
-        ${active
+    <a
+      href="/"
+      className={`group relative overflow-hidden 
+    ${active
         ? 'dark:bg-gray-800 bg-white text-amber-400 shadow-2xl shadow-gray-800/80'
-        : 'dark:text-gray-800 dark:hover:border-gray-800 text-white hover:border-white hover:border-solid hover:border hover:shadow-2xl hover:shadow-gray-800/80 cursor-pointer'} 
-        block rounded-b-lg px-3 py-2 text-base font-medium transition-all duration-300`}
+        : 'hover:shadow-2xl hover:shadow-gray-800/80 cursor-pointer'} 
+    block rounded-b-lg px-3 py-2 text-base font-medium transition-all duration-300`}
       aria-current="page"
     >
-      <motion.span className="w-100 h-100">{label}</motion.span>
-    </motion.a>
+      <div className={`absolute inset-0 w-0 bg-amber-400 dark:group-hover:bg-gray-800 transition-all duration-500 ease-out ${active ? '' : 'group-hover:w-full'}`} />
+      <span className={`relative ${active ? 'text-amber-400' : 'text-white group-hover:text-gray-800 dark:group-hover:text-white'}`}>{label}</span>
+    </a>
   );
 };
 
