70a18b54a46829b9f1787f0da8c060e642a8380c
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-18 01:35:51 +0300

    feat(navItemLG): use link instead of active to determine current page

--- src/components/navbar/NavItemLG.js
+++ src/components/navbar/NavItemLG.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useAtom } from 'jotai';
+import { pageAtom } from '../layout';
 
 // NavItemLG is a React component for rendering a navigation item for large screens.
 // It accepts props for label, active state, delay, and an icon.
-const NavItemLG = ({ label, active, delay, icon }) => {
+const NavItemLG = ({ label, delay, icon, link }) => {
+  const [page] = useAtom(pageAtom);
   return (
     <a
       href="/"
       className={`group relative overflow-hidden animate__animated animate__bounceInDown
-    ${active
+    ${page === `/${link}` || (link !== '' && page.includes(link))
         ? 'dark:bg-gray-800 bg-white text-amber-400 shadow-2xl shadow-gray-800/80'
         : 'shadow-2xl hover:shadow-gray-800/80 cursor-pointer'} 
     block rounded-b-lg px-3 py-2 text-base font-medium transition-all duration-300`}
@@ -15,11 +18,11 @@ const NavItemLG = ({ label, active, delay, icon }) => {
       style={{ animationDelay: `${1 + (0.3 * delay)}s` }}
     >
       <div className={`absolute inset-0 w-0 bg-amber-400 dark:group-hover:bg-gray-800 group-hover:bg-white transition-all duration-500 ease-out 
-        ${active ? '' : 'group-hover:w-full'}
+        ${page === `/${link}` || (link !== '' && page.includes(link)) ? '' : 'group-hover:w-full'}
         `}
       />
       <span className={`relative flex items-center justify-between
-        ${active ? 'text-amber-400' : 'text-white group-hover:text-gray-800 dark:group-hover:text-white duration-500 ease-in'}`}
+        ${page === `/${link}` || (link !== '' && page.includes(link)) ? 'text-amber-400' : 'text-white group-hover:text-gray-800 dark:group-hover:text-white duration-500 ease-in'}`}
       >
         <span className="text-lg mr-2">{icon}</span>
         <span>{label}</span>
