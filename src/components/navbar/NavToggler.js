0bb21f7ec7ceff0018032bfdae9557379273e584
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-18 01:38:46 +0300

    refactor(navToggler): change colors and background for light mode

--- src/components/navbar/NavToggler.js
+++ src/components/navbar/NavToggler.js
@@ -12,7 +12,7 @@ const NavToggler = () => {
       initial="hide"
       animate="show"
     >
-      <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+      <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-white hover:text-amber-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
         <span className="absolute -inset-0.5" />
         <span className="sr-only">Open main menu</span>
         <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
