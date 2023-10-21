cadbac3d48447b9902d18aba00b045253a04c214
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 02:01:37 +0300

    style(navLogo): center navLogo on small screens

--- src/components/navbar/NavLogo.js
+++ src/components/navbar/NavLogo.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NavLogo = () => {
   return (
-    <div className="flex flex-shrink-0 items-center m-auto">
+    <div className="flex flex-shrink-0 items-center nav-logo m-auto">
       <img
         className="h-11 w-11 animate__animated animate__rotateIn animate__delay-2s"
         src="/logo-tp.png"
