f4fd9adb8efad06808143e3cef64dd5dd9ad3688
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 01:39:10 +0300

    feat(toggleTheme): add icons in themeToggler

--- src/components/navbar/ThemeToggler.js
+++ src/components/navbar/ThemeToggler.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from '@headlessui/react';
 import { useAtom } from 'jotai';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import { themeAtom } from '../layout';
 
 const ThemeToggler = () => {
@@ -16,9 +17,18 @@ const ThemeToggler = () => {
         <span className="sr-only">Set theme</span>
         <span
           aria-hidden="true"
-          className={`${theme === 'dark' ? 'translate-x-9' : 'translate-x-0'}
-            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-amber-400 shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
+          className={`${theme === 'dark' ? 'translate-x-0 text-white' : 'translate-x-9 text-gray-800'} flex center absolute top-0 left-0 px-2
+            pointer-events-none h-[34px] w-[34px]transition duration-200 ease-in-out`}
+        >
+          {theme === 'dark' ? <MdLightMode /> : <MdDarkMode />}
+        </span>
+        <span
+          aria-hidden="true"
+          className={`${theme === 'dark' ? 'translate-x-9' : 'translate-x-0'} flex center
+            pointer-events-none h-[34px] w-[34px] transform rounded-full bg-amber-400 shadow-lg ring-0 transition duration-200 ease-in-out`}
+        >
+          {theme === 'dark' ? <MdDarkMode /> : <MdLightMode />}
+        </span>
       </Switch>
     </div>
   );
