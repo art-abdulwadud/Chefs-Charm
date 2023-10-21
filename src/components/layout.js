26d0d6173d32051dd189b96933403c6d2f1d8da7
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 02:04:51 +0300

    refactor(toggleTheme): replace themAtom with darkModeTheme in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,12 @@ import NavBar from './navbar/NavBar';
 export const isBrowser = typeof window !== 'undefined';
 export const pageAtom = atom(() => isBrowser ? window.location.pathname : '');
 export const themeAtom = atom(isBrowser && window.localStorage.getItem('themeSelected') && window.localStorage.getItem('themeSelected') === 'dark' ? 'dark' : '');
+export const darkModeAtom = atom(isBrowser && window.localStorage.getItem('darkMode'));
 
 const Layout = ({ children }) => {
-  const [theme] = useAtom(themeAtom);
+  const [darkMode] = useAtom(darkModeAtom);
   return (
-    <main className={`relative h-100vh z-10 ${theme}`}>
+    <main className={`relative h-100vh z-10 ${darkMode ? 'dark' : ''}`}>
       <div className="absolute h-100vh w-100 pattern-bg bg-white dark:bg-gray-800 bg-pattern-light dark:bg-pattern-dark dark z-0" />
       <NavBar />
       {children}
