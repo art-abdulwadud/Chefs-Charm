d16b9daee6a2c311799fbead1a2a5e0e6a2c5851
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 00:59:39 +0300

    feat(toggleTheme): add component for toggling theme

--- src/components/navbar/ThemeToggler.js
+++ src/components/navbar/ThemeToggler.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Switch } from '@headlessui/react';
+import { useAtom } from 'jotai';
+import { themeAtom } from '../layout';
 
 const ThemeToggler = () => {
+  const [theme, setTheme] = useAtom(themeAtom);
   return (
-    <div>ThemeToggler</div>
+    <div className="py-1">
+      <Switch
+        checked={theme === 'dark'}
+        onChange={() => theme === 'dark' ? setTheme('') : setTheme('dark')}
+        className={`${theme === 'dark' ? 'bg-teal-900' : 'bg-teal-700'}
+          relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span className="sr-only">Set theme</span>
+        <span
+          aria-hidden="true"
+          className={`${theme === 'dark' ? 'translate-x-9' : 'translate-x-0'}
+            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+        />
+      </Switch>
+    </div>
   );
 };
 
