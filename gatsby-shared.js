578d80abf28dbd75772a8c465b42a32833f450ba
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 04:24:26 +0300

    wrap root element with react-query provider

--- gatsby-shared.js
+++ gatsby-shared.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Provider } from 'jotai';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
 
 export const wrapRootElement = ({ element }) => {
   return (
     <Provider>
-      {element}
+      <QueryClientProvider client={queryClient}>
+        {element}
+      </QueryClientProvider>
     </Provider>
   );
 };
