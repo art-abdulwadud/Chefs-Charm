4771852947ae9e778ea777cc68516d6d9175b014
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 04:35:10 +0300

    remove react-query provider from gatsby-shared

--- gatsby-shared.js
+++ gatsby-shared.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Provider } from 'jotai';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-export const queryClient = new QueryClient();
 
 export const wrapRootElement = ({ element }) => {
   return (
     <Provider>
-      <QueryClientProvider client={queryClient}>
-        {element}
-      </QueryClientProvider>
+      {element}
     </Provider>
   );
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { atom, useAtom } from 'jotai';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'animate.css/animate.min.css';
 import './layout.css';
 import './fonts.css';
 import NavBar from './navbar/NavBar';
 
+export const queryClient = new QueryClient();
 export const isBrowser = typeof window !== 'undefined';
 // A global state for identifying the current page
 export const pageAtom = atom(() => isBrowser ? window.location.pathname : '');
@@ -20,11 +22,13 @@ const Layout = ({ children }) => {
     if (isBrowser) window.localStorage.setItem('darkMode', darkMode);
   }, [darkMode]);
   return (
-    <main className={`relative h-100vh z-10 ${darkMode ? 'dark' : ''}`}>
-      <div className="absolute h-100vh w-100 pattern-bg bg-white dark:bg-gray-800 bg-pattern-light dark:bg-pattern-dark dark z-0" />
-      <NavBar />
-      {children}
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className={`relative h-100vh z-10 ${darkMode ? 'dark' : ''}`}>
+        <div className="absolute h-100vh w-100 pattern-bg bg-white dark:bg-gray-800 bg-pattern-light dark:bg-pattern-dark dark z-0" />
+        <NavBar />
+        {children}
+      </main>
+    </QueryClientProvider>
   );
 };
 
