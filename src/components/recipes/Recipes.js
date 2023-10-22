b781f19fef44da5f53260288974e4aa90fdcd74f
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-22 04:43:07 +0300

    refactor(recipes): use react-query to fetch recipes

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,40 +1,34 @@
 import axios from 'axios';
-import { atom, useAtom } from 'jotai';
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import RecipeCard from './RecipeCard';
 
-export const recipesAtom = atom([]);
-
 const Recipes = () => {
-  const [recipes, setRecipes] = useAtom(recipesAtom);
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const options = {
-          method: 'GET',
-          url: 'https://tasty.p.rapidapi.com/recipes/list',
-          params: {
-            from: '0',
-            size: '20',
-            tags: 'under_30_minutes'
-          },
-          headers: {
-            'X-RapidAPI-Key': process.env.GATSBY_API_KEY,
-            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-          }
-        };
-        const response = await axios.request(options);
-        setRecipes(response.data?.results);
-      } catch (error) {
-        console.log(error.message);
-        setRecipes([]);
-      }
-    };
-    fetchRecipes();
-  }, []);
+  const { data } = useQuery(['fetchUsers'], async () => {
+    try {
+      const options = {
+        method: 'GET',
+        url: 'https://tasty.p.rapidapi.com/recipes/list',
+        params: {
+          from: '0',
+          size: '20',
+          tags: 'under_30_minutes'
+        },
+        headers: {
+          'X-RapidAPI-Key': process.env.GATSBY_API_KEY,
+          'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+      };
+      const response = await axios.request(options);
+      return response.data?.results;
+    } catch (error) {
+      console.log(error.message);
+      return [];
+    }
+  });
   return (
     <div>
-      {recipes.map((key) => (
+      {data?.map((key) => (
         <RecipeCard key={key.id} />
       ))}
     </div>
