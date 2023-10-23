c81b6d7cd2e6e8d45c09f164bb4c7136b7323e99
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-23 15:55:16 +0300

    feat(recipes): use dummy data instead of fetching just until styling is finished

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -5,8 +5,8 @@ import { selectedRecipeAtom } from './Recipes';
 const RecipeCard = ({ recipe }) => {
   const [selectedRecipe] = useAtom(selectedRecipeAtom);
   return (
-    <div className="h-28 w-28 z-3">
-      <img src={recipe?.thumbnail_url} alt={recipe?.slug || ''} className="h-28 w-28" />
+    <div className="h-52 w-28">
+      <img src={recipe?.thumbnail_url} alt={recipe?.slug || ''} className="h-52 w-28" />
     </div>
   );
 };
--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,29 +1,31 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { atom } from 'jotai';
 import RecipeCard from './RecipeCard';
+import { dummyData } from './dummyData';
 
 export const selectedRecipeAtom = atom(null);
 
 const Recipes = () => {
-  const { data } = useQuery(['fetchUsers'], async () => {
+  const { data } = useQuery(['fetchUsers'], () => {
     try {
-      const options = {
-        method: 'GET',
-        url: 'https://tasty.p.rapidapi.com/recipes/list',
-        params: {
-          from: '0',
-          size: '20',
-          tags: 'under_30_minutes'
-        },
-        headers: {
-          'X-RapidAPI-Key': process.env.GATSBY_API_KEY,
-          'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-        }
-      };
-      const response = await axios.request(options);
-      return response.data?.results;
+      // const options = {
+      //   method: 'GET',
+      //   url: 'https://tasty.p.rapidapi.com/recipes/list',
+      //   params: {
+      //     from: '0',
+      //     size: '20',
+      //     tags: 'under_30_minutes'
+      //   },
+      //   headers: {
+      //     'X-RapidAPI-Key': process.env.GATSBY_API_KEY,
+      //     'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+      //   }
+      // };
+      // const response = await axios.request(options);
+      // return response.data?.results;
+      return dummyData;
     } catch (error) {
       console.log(error.message);
       return [];
