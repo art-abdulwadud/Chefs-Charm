965c4ede4432629f51e740a33c70be3b39314c9d
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-23 17:50:53 +0300

    style(recipeCard): style and display name and description

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -6,22 +6,27 @@ const RecipeCard = ({ recipe }) => {
   // const [selectedRecipe] = useAtom(selectedRecipeAtom);
   return (
     <div className="relative h-80 w-80 shadow-2xl cursor bg-amber-400 flex center" style={{ borderRadius: '20px' }}>
-      <img src={recipe?.thumbnail_url} alt={recipe?.slug || ''} className="h-72 w-72" style={{ borderRadius: '50%' }} />
+      <img src={recipe?.thumbnail_url} alt={recipe?.slug || ''} className="h-72 w-72 shadow-2xl" style={{ borderRadius: '50%', borderBottomLeftRadius: '5px', borderBottomRightRadius: '5px' }} />
       <div id="preview-info" className="absolute bottom-3 left-0 h-40 w-100 px-3">
         <div
-          className="h-36 p-2"
+          className="h-36 p-2 shadow-xl"
           style={{
-            background: 'rgba(255,255,255,0.3)',
+            background: 'rgba(0,0,0,0.3)',
             backdropFilter: 'blur(4px)',
             WebkitBackdropFilter: 'blur(4px)',
-            borderRadius: '20px' }}
+            borderRadius: '10px' }}
         >
-          <div className="w-100 flex justify-between text-xs text-gray-900">
+          <div className="w-100 flex justify-between text-xs text-white">
             <span>{recipe?.credits?.map((key) => (
               <span key={key.id}>{key.name}</span>
             ))}
             </span>
-            <span className="font-light text-gray-700">{recipe?.updated_at ? new Date(recipe.updated_at).toDateString() : new Date().toDateString()}</span>
+            <span className="font-light text-gray-100">{recipe?.updated_at ? new Date(recipe.updated_at).toDateString() : new Date().toDateString()}</span>
+          </div>
+          <div className="mt-3 text-white">
+            <span className="font-medium text-base">{recipe?.name}</span>
+            <br />
+            <span className="font-light text-xs short-text">{recipe?.description}</span>
           </div>
         </div>
       </div>
