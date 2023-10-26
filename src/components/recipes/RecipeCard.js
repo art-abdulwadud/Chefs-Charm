057ebef5a1e6cbb3f83c5ae98c3a97d958308bf3
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-10-26 18:43:39 +0300

    anmate background color of recipeCard

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -10,13 +10,13 @@ import { selectedRecipeAtom } from './Recipes';
 const RecipeCard = ({ recipe, delay }) => {
   const [selectedRecipe] = useAtom(selectedRecipeAtom);
   const recipeCardVariants = {
-    hidden: { width: 0, maxHeight: '0', minHeight: '0', padding: '0 0', transition: { ease: 'easeInOut' } },
-    preview: { maxHeight: '20rem', minHeight: '20rem', width: '20rem', padding: '1rem 1rem', transition: { ease: 'easeInOut', duration: 1 } },
-    expand: { maxHeight: '1000px', minHeight: '100vh', width: '100vw', padding: '1rem 1rem', transition: { ease: 'easeInOut', duration: 1 } }
+    hidden: { width: 0, maxHeight: '0', minHeight: '0', padding: '0 0', backgroundColor: 'rgba(251, 191, 36, 0)', transition: { ease: 'easeInOut' } },
+    preview: { maxHeight: '20rem', minHeight: '20rem', width: '20rem', padding: '1rem 1rem', backgroundColor: 'rgba(251, 191, 36, 1)', transition: { ease: 'easeInOut', duration: 1 } },
+    expand: { maxHeight: '1000px', minHeight: '100vh', width: '100vw', padding: '1rem 1rem', backgroundColor: 'rgba(251, 191, 36, 0)', transition: { ease: 'easeInOut', duration: 1 } }
   };
   return (
     <motion.div
-      className={`relative group bg-amber-400 flex justify-start duration-500 animate__animated animate__rotateInUpRight overflow-hidden
+      className={`relative group flex justify-start duration-500 animate__animated animate__rotateInUpRight overflow-hidden
       ${selectedRecipe?.id === recipe?.id ? '' : !selectedRecipe ? '' : 'animate__rotateOutUpRight'}`}
       style={{
         borderRadius: '20px',
