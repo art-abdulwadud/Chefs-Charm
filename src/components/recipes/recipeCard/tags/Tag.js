38d110f80f9e211045f0fb79f95feb9cbef27a27
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-11-04 20:37:04 +0300

    animate tags

--- src/components/recipes/recipeCard/tags/Tag.js
+++ src/components/recipes/recipeCard/tags/Tag.js
@@ -2,18 +2,18 @@ import { useAtom } from 'jotai';
 import React from 'react';
 import { selectedTagAtom } from '../../Recipes';
 
-const Tag = ({ label }) => {
+const Tag = ({ label, delay }) => {
   const [selectedTag, setSelectedTag] = useAtom(selectedTagAtom);
   return (
     <li
       className={`
       ${selectedTag && selectedTag === label ? 'text-white dark:text-gray-800 bg-amber-400'
-        : `
-       bg-white text-gray-800 dark:bg-gray-800 dark:text-white
-       `}
+        : 'bg-white text-gray-800 dark:bg-gray-800 dark:text-white'}
+        animate__animated animate__bounceInDown
        p-2 rounded-lg shadow-md shadow-gray-800/40 dark:shadow-white/10
       cursor
       `}
+      style={{ animationDelay: `${0.3 * delay}s` }}
       onClick={() => setSelectedTag(label)}
     >
       #{label}
--- src/components/recipes/recipeCard/tags/Tags.js
+++ src/components/recipes/recipeCard/tags/Tags.js
@@ -4,12 +4,12 @@ import Tag from './Tag';
 const Tags = () => {
   return (
     <ul className="relative list-none flex flex-nowrap overflow-x-auto space-x-2 md:justify-center py-2 mt-2 z-10" style={{ maxWidth: '100%' }}>
-      <Tag label="breakfast" />
-      <Tag label="brunch" />
-      <Tag label="under_30_minutes" />
-      <Tag label="mothers_day" />
-      <Tag label="valentines_day" />
-      <Tag label="comfort_food" />
+      <Tag label="brunch" delay={0} />
+      <Tag label="breakfast" delay={1} />
+      <Tag label="under_30_minutes" delay={2} />
+      <Tag label="mothers_day" delay={3} />
+      <Tag label="valentines_day" delay={4} />
+      <Tag label="comfort_food" delay={5} />
     </ul>
   );
 };
