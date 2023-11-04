a137a8f8232259b456b244e3dc8196b884c1e50e
art-abdulwadud <abdulwadudhajike@gmail.com>
2023-11-04 19:28:33 +0300

    style tag component and add click event listener to it

--- src/components/recipes/recipeCard/tags/Tag.js
+++ src/components/recipes/recipeCard/tags/Tag.js
@@ -1,11 +1,20 @@
+import { useAtom } from 'jotai';
 import React from 'react';
+import { selectedTagAtom } from '../../Recipes';
 
 const Tag = ({ label }) => {
+  const [selectedTag, setSelectedTag] = useAtom(selectedTagAtom);
   return (
-    <li className={`
-    text-white dark:text-gray-800 p-2 
-    bg-amber-400 rounded-lg
-    `}
+    <li
+      className={`
+      ${selectedTag && selectedTag === label ? 'text-white dark:text-gray-800 bg-amber-400'
+        : `
+       bg-white text-gray-800 dark:bg-gray-800 dark:text-white
+       `}
+       p-2 rounded-lg shadow-md shadow-gray-800/40 dark:shadow-white/10
+      cursor
+      `}
+      onClick={() => setSelectedTag(label)}
     >
       #{label}
     </li>
