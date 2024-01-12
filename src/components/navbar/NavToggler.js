e1717953400861e0af29cd0dcda56d9a60d41d58
art-abdulwadud <abdulwadudhajike@gmail.com>
2024-01-12 20:41:06 +0300

    make navtoggler icon a search icon

--- src/components/navbar/NavToggler.js
+++ src/components/navbar/NavToggler.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAtom } from 'jotai';
+import { FaSearch } from 'react-icons/fa';
 import { navOpenedAtom } from './NavBar';
 
 const NavToggler = () => {
@@ -17,12 +18,7 @@ const NavToggler = () => {
       >
         <span className="absolute -inset-0.5" />
         <span className="sr-only">Open main menu</span>
-        <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-        <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-        </svg>
+        <FaSearch className="block h-6 w-6" />
       </button>
     </div>
   );
